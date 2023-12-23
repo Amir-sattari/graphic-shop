5b275455c7b69297c0e11dddd78aaed8d390efac
Amir Hossein <am.h.sattari@gmail.com>
2023-12-23 19:37:26 +0330

    admin base blade implemented

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('products/all',function (){
     return view('frontend.products.all');
 });
+
+Route::get('admin/index',function (){
+    return view('admin.index');
+});
+
+Route::get('admin/users',function (){
+    return view('admin.users.index');
+});
+
