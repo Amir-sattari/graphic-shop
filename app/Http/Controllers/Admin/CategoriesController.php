18dd3b70eb2536e366eb4b30890cf2c8fb13a6ec
Amir Hossein <am.h.sattari@gmail.com>
2023-12-24 18:34:11 +0330

    pagination implemented

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -31,7 +31,7 @@ class CategoriesController extends Controller
 
     public function all()
     {
-        $categories = Category::all();
+        $categories = Category::paginate(10);
         return view('admin.categories.all',compact('categories'));
     }
 }
